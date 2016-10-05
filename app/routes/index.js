f5271cf6b2bc20c2032e6d5777c8bad5068368c2
Shubham Sharma <shubham.sharma0677@gmail.com>
2016-10-05 16:20:05 +0530

    first commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 module.exports = function(app) {
-	app.routes('/').get(function(req,resp) {
+	app.route('/').get(function(req,resp) {
 		var ip = req.headers['x-forwarded-for'] ||
 				req.connection.remoteAddress;
 
 		var info = {
 				'ip-address': ip,
 				'language': req.headers['accept-language'].split(',')[0],
-				'software': req.headers[user-agent].split(') ')[0].split(' (')[1]
+				'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
 				};
 		resp.send(info);
 });
